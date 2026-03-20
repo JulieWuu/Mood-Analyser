def37f5428991a8fd755da656948e89009f35c39
Julia Wu <juliaaaaawu8@gmail.com>
2026-03-20 17:17:30 +0000

    Trim DONT_CARE_LIST to minimal entries
    Reduced DONT_CARE_LIST to essential values.

--- TinyDB_of_Mood.py
+++ TinyDB_of_Mood.py
@@ -2,43 +2,7 @@ import json
 
 from tinydb import TinyDB, Query
 
-DONT_CARE_LIST = ["", "1", "2", "22", "3", "4", "5",
-                      "a", "about", "abs", "absolutely", "activities", "actually", "after", "afternoon", "again",
-                      "ago", "ah", "all", "almost", "already", "also", "am", "an", "and", "another", "anymore",
-                      "anymore", "anyway", "are", "around", "arrived", "as", "asked", "at",
-                      "back", "bag", "be", "been", "before", "being", "believe", "better", "big", "bit", "bought",
-                      "but", "buy", "buying", "by",
-                      "came", "can", "cannot", "cant", "check", "choice", "clothes", "could", "couldnt",
-                      "damn", "day", "days", "de", "decided", "did", "didnt", "dinner", "do", "does", "doing", "done",
-                      "dont", "down",
-                      "early", "enough", "enter", "even", "evening", "ever", "every", "everything", "end", "ending",
-                      "falling", "feel", "feeling", "felt", "feels", "final", "finally", "finished", "finishing",
-                      "first", "food", "for", "forgot", "formal", "found", "free", "from",
-                      "gave", "get", "getting", "go", "going", "gone", "gonna", "got", "gotta",
-                      "had", "have", "having", "here", "hour", "hours", "he", "him", "his", "how", "her", "hers",
-                      "i", "ias", "idk", "if", "ill", "im", "in", "instead", "into", "irl", "is", "it", "its", "ive",
-                      "just",
-                      "keep", "know",
-                      "la", "last", "late", "least", "like", "liked", "literally", "little", "long", "looking", "losing",
-                      "lot", "love", "loving", "lunch",
-                      "made", "making", "many", "me", "meet", "meeting", "might", "month", "more", "morning", "much",
-                      "must", "my", "myself",
-                      "n", "need", "never", "new", "next", "night", "no", "not", "now",
-                      "of", "off", "oh", "on", "once", "one", "only", "open", "or", "our", "out", "over", "overall", "own",
-                      "paper", "papers", "part", "past", "people", "picked",
-                      "quite",
-                      "ready", "really", "right",
-                      "said", "say", "see", "seeing", "set", "shes", "should", "since", "skip", "skipped", "sleep", "slept",
-                      "slightly", "so", "some", "someone", "soon", "spent", "stand", "start", "started", "starting", "stay",
-                      "still", "stop", "stuff", "such", "suddenly", "show", "second",
-                      "taking", "than", "that", "the", "them", "then", "there", "these", "thing", "things", "think", "this",
-                      "tho", "though", "thought", "thoughts", "till", "time", "timed", "to", "today", "tomorrow", "tonight",
-                      "too", "try", "trying", "those",
-                      "uhoh", "until", "up", "use", "using",
-                      "very",
-                      "waiting", "wakeup", "wanna", "want", "wanted", "was", "wasnt", "way", "we", "week", "well", "went",
-                      "what", "when", "where", "whole", "why", "with", "without", "woke", "would", "writing", "wrong", "wrote",
-                      "yesterday", "your"]
+DONT_CARE_LIST = ["", "1", "2", "22", "3", "4", "5"]
 
 class TinyDBofMood:
 
